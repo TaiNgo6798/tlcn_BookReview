bec2a0f410cc3b369e4617d816964e9d8c2511f4
Vũ Tấn Đạt <33850332+datvu1061998@users.noreply.github.com>
2019-11-11 02:21:36 +0700

    create model review

---
+++ api_book_review/ReviewBook/model/review.js
@@ -0,0 +1,32 @@
+class Review{
+    constructor(
+        title,
+        kind,
+        urlImage,
+        nameImage,
+        desc,
+        uid,
+        name
+    ){
+        this.title = title,
+        this.kind = kind,
+        this.urlImage = urlImage,
+        this.nameImage = nameImage,
+        this.desc = desc,
+        this.uid = uid,
+        this.name = name,
+        this.numberTime = Date.now();
+        this.time = new Date().toLocaleString("vi", {
+            hour: "numeric",
+            minute: "numeric"
+        }),
+        this.Date = new Date().toLocaleDateString("vi", {
+            weekday: "long",
+            month: "long",
+            day: "2-digit",
+            year: "numeric"
+        })
+    }
+}
+
+module.exports = Review;
---
+++ api_book_review/ReviewBook/model/user.js
@@ -0,0 +1,19 @@
+class User{
+    constructor(
+        firstName,
+        secondName,
+        gender,
+        phone,
+        birthday,
+        email
+    ){
+        this.firstName = firstName,
+        this.secondName = secondName,
+        this.gender = gender,
+        this.phone = phone,
+        this.birthday = new Date(birthday).toLocaleDateString('vi');
+        this.email = email,
+        this.image = new String()
+    }
+}
+module.exports = User;
