3eda6c8b4e160547272d11098b5709ec816b610d
Vũ Tấn Đạt <33850332+datvu1061998@users.noreply.github.com>
2019-11-28 02:03:17 +0700

    api get post comment

--- api_book_review/ReviewBook/apiRouter/commentRouter.js
+++ api_book_review/ReviewBook/apiRouter/commentRouter.js
@@ -3,48 +3,67 @@ var express = require("express");
 var Comment = require("./../model/comment");
 
 const commentRouter = express.Router();
-const user = firebase.auth().currentUser;
 commentRouter
-  .route("/comment")
+  .route("/review/comment/:review_id")
   //luu 1 comment cua user vao co so du lieu
   .post((req, res) => {
     var body = req.body.body;
     var imageUser = req.body.imageUser;
     var nameUser = req.body.nameUser;
-    var review_id = req.body.review_id;
-    var comment = new Comment(user.uid, body, imageUser, nameUser);
+    var review_id = req.params.review_id;
+
+    const userID = firebase.auth().currentUser.uid;
+    var comment = new Comment(userID, body, imageUser, nameUser);
     dbCommentRef = firebase
       .database()
       .ref()
-      .child("Comments");
+      .child("Comments")
+      .child(review_id);
     newCommentRef = dbCommentRef.push();
     newCommentRef.set(comment, error => {
       if (error) {
         var errorMessage = error.message;
         res.send(errorMessage);
       } else {
-            dbReview = firebase.database().ref().child('Reviews').child(review_id).child('comments');
-            dbReview.once('value',snapshot=>{
-                var key = newCommentRef.key;
-                var comments = [key];
-                if(snapshot.exists()){
-                    comments = Object.values(snapshot.val());
-                    comments.unshift(key);
-                }
-                dbReview.set(comments,error=>{
-                    if (error) {
-                        var errorMessage = error.message;
-                        res.send(errorMessage);
-                      } else {
-                        res.send({ 
-                          success: true,
-                          message: "comment review successful"
-                        });
-                      }
-                })
-            })
+        dbReview = firebase
+          .database()
+          .ref()
+          .child("Reviews")
+          .child(review_id)
+          .child("comments");
+        var key = newCommentRef.key;
+        dbReview.update({ [key]: true }, error => {
+          if (error) {
+            var errorMessage = error.message;
+            res.send(errorMessage);
+          } else {
+            res.send({
+              success: true,
+              message: "Commented successfully"
+            });
+          }
+        });
       }
     });
-  });
+  })
+  // TRẢ VỀ TẤT CẢ COMMMENT CỦA BÀI VIẾT
+  .get((req,res)=>{
+    var review_id = req.params.review_id;
+    dbCommentRef = firebase
+    .database()
+    .ref()
+    .child("Comments")
+    .child(review_id);
+    dbCommentRef.on('value',snapshot=>{
+      if(snapshot.exists()){
+        res.send(snapshot.val());
+      }else{
+        res.send({
+          success:false,
+          message:"no exists"
+        })
+      }
+    })
+  })
 
 module.exports = commentRouter;
--- api_book_review/ReviewBook/model/review.js
+++ api_book_review/ReviewBook/model/review.js
@@ -8,22 +8,19 @@ class Review{
         uid,
         name
     ){
-        this.title = title,
         this.kind = kind,
         this.urlImage = urlImage,
         this.nameImage = nameImage,
         this.desc = desc,
         this.uid = uid,
-        this.name = name,
+        this.name = name,   
         this.likes = new Array();
         this.comments = new Array();
         this.shares = new Array();
         this.numberTime = Date.now();
         this.time = new Date().toLocaleString("vi", {
             hour: "numeric",
-            minute: "numeric"
-        }),
-        this.date = new Date().toLocaleDateString("vi", {
+            minute: "numeric",
             weekday: "long",
             month: "long",
             day: "2-digit",
